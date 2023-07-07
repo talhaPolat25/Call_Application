1eb59eebb3e6d316cfdc61e0c7afff6f18e37cc1
talhaPolat25 <easynick25@gmail.com>
2023-07-07 10:49:59 +0300

    Initial Commit

---
+++ Call_Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Call_Application
+//
+//  Created by talha polat on 7.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
